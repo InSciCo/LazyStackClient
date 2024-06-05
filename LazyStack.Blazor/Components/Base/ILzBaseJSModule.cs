9d88e45f7c8bb42930eb7635493785caac5c854c
Timothy May <tmay@inscico.com>
2024-06-05 14:19:06 -0700

    Added LzBaseJSModule

---
+++ LazyStack.Blazor/Components/Base/ILzBaseJSModule.cs
@@ -0,0 +1,15 @@
+﻿
+namespace LazyStack.Blazor;
+
+public interface ILzBaseJSModule
+{
+    /// <summary>
+    /// Gets the module file name.
+    /// </summary>
+    string ModuleFileName { get; }
+
+    /// <summary>
+    /// Gets the awaitable <see cref="IJSObjectReference"/> task.
+    /// </summary>
+    Task<IJSObjectReference> Module { get; }
+}
